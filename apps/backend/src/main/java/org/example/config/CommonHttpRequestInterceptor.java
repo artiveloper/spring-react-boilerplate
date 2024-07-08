7e9d399ca18faa9cf0bd581db70d8c665b98bfa8
Artiveloper <artiveloper@gmail.com>
2024-07-08 23:15:22 +0900

    feat: 로깅에 MDC를 추가하고 로깅 포맷을 변경한다.

---
+++ apps/backend/src/main/java/org/example/config/CommonHttpRequestInterceptor.java
@@ -0,0 +1,35 @@
+package org.example.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class CommonHttpRequestInterceptor implements AsyncHandlerInterceptor {
+
+    public static final String HEADER_REQUEST_UUID_KEY = "x-request-id";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String requestEventId = request.getHeader(HEADER_REQUEST_UUID_KEY);
+        if (!StringUtils.hasLength(requestEventId)) {
+            requestEventId = UUID.randomUUID().toString();
+        }
+
+        MDC.put(HEADER_REQUEST_UUID_KEY, requestEventId);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        MDC.remove(HEADER_REQUEST_UUID_KEY);
+    }
+
+}
